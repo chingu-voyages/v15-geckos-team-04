037f141224d5024f8cda6cd593176bbf5d9e58d7
user <ovg43@yandex.ru>
2020-01-17 19:21:16 +0300

    add scaleButton element not designed

--- src/containers/MainWeatherSection.jsx
+++ src/containers/MainWeatherSection.jsx
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
+import defaultData from "../data/defaultData";
+import ScaleButton from "../components/scaleButton"
+
+const {sol_keys} = defaultData;
+
 
 class MainWeatherSection extends Component {
   constructor(props) {
     super(props);
-    this.state = { }
+    this.state = {
+      sol: [],
+      temperature: [],
+      windSpeed: [],
+      pressure: [],
+      season: ""
+     }
   };
+ 
+  
 
+  componentDidMount() {
+    this.setState ({
+      sol: sol_keys,
+      
+    })
+
+  }
 
   render() {
-      return (<div className="text-center" >Main Weather Section</div>)
+    
+      return (<div className="text-center"> Main Weather Section 
+      <ScaleButton /></div>);
   }}
 
   export default MainWeatherSection;
