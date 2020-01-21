c17104a3ece58e94bf1326d31d03c9c6f602cce9
Aloys Berger <aloysberger@gmail.com>
2020-01-21 20:30:38 +0100

    Resolve conflicts with git stash

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import Footer from "./containers/Footer";
 import Mission from './containers/Mission';
 import {Container} from "react-bootstrap";
 
-//Style sheet required for react-bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
--- src/containers/Mission.jsx
+++ src/containers/Mission.jsx
@@ -9,80 +9,89 @@
 
 import React from 'react';
 import {Container, Row, Col, Accordion, Card, Button} from 'react-bootstrap';
+import './Mission.css';
 
 function Mission() {
   return (
-    <Container className = 'mission_wrapper' style = { { 
-      backgroundImage: "url('https://images.unsplash.com/photo-1547234935-80c7145ec969?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1653&q=80')" ,
-      backgroundSize: 'cover',
-    } }> 
+    <Container className = 'mission_wrapper'> 
     <Row style = {{
         height: '100vh',
+        width: '100vw',
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-around',
       }}>
-      <Col md = {12} lg = {6}>
+      <Col md = {12} lg = {7} style = {{
+        
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        height: '56.25%',
+        width: '100%',
+      }}>
         <div className = 'mission_video' 
         style = {{
-          width: '100%',
-          height: '56.25%',
+          display: 'flex',
+          justifyContent: 'center',
+          width: '100%'
         }}
         >
-        <iframe src = 'https://youtube.com/embed/LKLITDmm4NA' 
+        <iframe src = 'https://youtube.com/embed/LKLITDmm4NA'
+        allowfullscreen = 'true' 
         style = {{
-          display: 'block',
-          height: '100%',
+          flexBasis: '100%',
           width: '100%'
         }}
         />
         </div>
       </Col>
 
-      <Col md = {12} lg = {6}>
-      <div className = 'mission_information'>
-        <Accordion defaultActiveKey = '0'>
+      <Col md = {12} lg = {5}>
+      <div className = 'mission_information' style = {{ width: '100%' }}>
+        <Accordion defaultActiveKey = ''>
         <Card>
           <Card.Header>
-            <Accordion.Toggle as = {Button} variant='link' eventKey = '0'>
-              Insight mission
+            <Accordion.Toggle as = {Button} variant='light' eventKey = '0'>
+              InSight — Studying the 'Inner Space' of Mars
             </Accordion.Toggle>
           </Card.Header>
           <Accordion.Collapse eventKey = '0'>
             <Card.Body>
-              The main purpose of the Insight mission is to get more knowledge about the composition of Mars.
-              InSight — Studying the 'Inner Space' of Mars
-              InSight, short for Interior Exploration using Seismic Investigations, Geodesy and Heat Transport, is a Mars lander designed to give the Red Planet its first thorough checkup since it formed 4.5 billion years ago. It is the first outer space robotic explorer to study in-depth the "inner space" of Mars: its crust, mantle, and core.
+              <p>The main purpose of the Insight mission is to get more knowledge about the composition of Mars.</p>
+              <p><strong>InSight</strong>, short for <strong>I</strong>nterior <strong>E</strong>xploration using <strong>S</strong>eismic <strong>I</strong>nvestigations, <strong>G</strong>eodesy and <strong>H</strong>eat <strong>T</strong>ransport, is a Mars lander designed to give the Red Planet its first thorough checkup since it formed 4.5 billion years ago.</p>
+              <p>It is the first outer space robotic explorer to study in-depth the "inner space" of Mars: its crust, mantle, and core.</p>
             </Card.Body>
           </Accordion.Collapse>
         </Card>
 
               <Card>
           <Card.Header>
-            <Accordion.Toggle as = {Button} variant='link' eventKey = '1'>
+            <Accordion.Toggle as = {Button} variant='light' eventKey = '1'>
               Weather Data
             </Accordion.Toggle>
           </Card.Header>
           <Accordion.Collapse eventKey = '1'>
             <Card.Body>
-              The weather information above are provided by NASA's Insight Mars lander.
-              NASA’s InSight Mars lander takes continuous weather measurements (temperature, wind, pressure) on the surface of Mars at Elysium Planitia, a flat, smooth
-              plain near Mars’ equator.   
+              <p>The weather information above are provided by NASA's Insight Mars lander.</p>
+              <p>NASA’s InSight Mars lander takes continuous weather measurements (temperature, wind, pressure) on the surface of Mars at Elysium Planitia, a flat, smooth
+              plain near Mars’ equator.</p>
+              <p>InSight also measures tectonic activity and meteorite impacts on Mars today.</p>
             </Card.Body>
           </Accordion.Collapse>
         </Card>
 
               <Card>
           <Card.Header>
-            <Accordion.Toggle as = {Button} variant='link' eventKey = '2'>
+            <Accordion.Toggle as = {Button} variant='light' eventKey = '2'>
               Interior of Mars
             </Accordion.Toggle>
           </Card.Header>
           <Accordion.Collapse eventKey = '2'>
             <Card.Body>
-              Mars' Interior: Artist's rendition showing the inner structure of Mars. The topmost layer is known as the crust, underneath it is the mantle, which rests on a solid inner core.
-              Studying Mars' interior structure answers key questions about the early formation of rocky planets in our inner solar system - Mercury, Venus, Earth, and Mars - more than 4 billion years ago, as well as rocky exoplanets. InSight also measures tectonic activity and meteorite impacts on Mars today.
-              The lander uses cutting edge instruments, to delve deep beneath the surface and seek the fingerprints of the processes that formed the terrestrial planets. It does so by measuring the planet's "vital signs": its "pulse" (seismology), "temperature" (heat flow), and "reflexes" (precision tracking).
-              This mission is part of NASA's Discovery Program for highly focused science missions that ask critical questions in solar system science.  
+              <p>Studying Mars' interior structure answers key questions about the early formation of rocky planets in our inner solar system - Mercury, Venus, Earth, and Mars - more than 4 billion years ago, as well as rocky exoplanets.</p>
+              <p>The lander uses cutting edge instruments, to delve deep beneath the surface and seek the fingerprints of the processes that formed the terrestrial planets.
+              It does so by measuring the planet's "vital signs": its "pulse" (seismology), "temperature" (heat flow), and "reflexes" (precision tracking).</p>
+              <p>This mission is part of NASA's Discovery Program for highly focused science missions that ask critical questions in solar system science.</p>
             </Card.Body>
           </Accordion.Collapse>
         </Card>
