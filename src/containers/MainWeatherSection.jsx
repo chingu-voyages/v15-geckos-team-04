f597a4dd4a8614c5c92a80ccbe37c89bb49e45f3
user <ovg43@yandex.ru>
2020-01-23 20:03:46 +0300

    connect to Nasa api

--- src/containers/MainWeatherSection.jsx
+++ src/containers/MainWeatherSection.jsx
@@ -5,17 +5,40 @@ import SevenDaysForecast from "./SevenDaysForecast"
 
 const {sol_keys} = defaultData;
 const defData = Object.values(defaultData).filter(i=>i.AT);
+const key = "4eyqnsnfMzgXvo5el4jgj6pKMB9YaJVyiVTssCge";
 
 class MainWeatherSection extends Component {
   constructor(props) {
     super(props);
     this.state = {
       defData: defData,
-      sol: sol_keys}
+      sol: sol_keys,
+      isLoaded: false,
+      error: null}
   };
  
   
- componentDidMount() {}
+ componentDidMount() {
+   fetch(
+  "https://api.nasa.gov/insight_weather/?api_key=4eyqnsnfMzgXvo5el4jgj6pKMB9YaJVyiVTssCge&feedtype=json&ver=1.0"
+)
+  .then(response => response.json())
+  .then(
+    result => {
+      this.setState({
+        isLoaded: true,
+        defData: Object.values(result).filter(i=>i.AT),
+        sol: result.sol_keys
+      });
+      console.log(this.state);
+    },
+    error => {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    }
+  );}
 
   render() {
       const {defData, sol} = this.state;
