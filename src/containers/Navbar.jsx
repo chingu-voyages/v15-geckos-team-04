f0cc041095f2625b2b542af45c9a2b786599ee5c
Ana Segarra <anasegarraber@gmail.com>
2020-01-26 01:48:11 +0100

    Add smoother scrolling and mark active link

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -7,22 +7,22 @@ const Navbar = () => {
 		<Container className="navbar">
 		<Row className="justify-content-end">
 			<Col md="auto" xs="6">
-				<Link to="weather" smooth={true} className="link">
+				<Link to="weather" spy={true} smooth={true} duration={2000} className="link">
 					Current Weather
 				</Link>
 			</Col>
 			<Col md="auto" xs="6">
-				<Link to="forecast" smooth={true} className="link">
+				<Link to="forecast"  spy={true} smooth={true} duration={2000} className="link">
 					7-Days Forecast
 				</Link>
 			</Col>
 			<Col md="auto" xs="6">
-				<Link to="reports" smooth={true} className="link">
+				<Link to="reports"  spy={true} smooth={true} duration={2000} className="link">
 					Weather Reports
 				</Link>
 			</Col>
 			<Col md="auto" xs="6">
-				<Link to="mission" smooth={true} className="link">
+				<Link to="mission"  spy={true} smooth={true} duration={2000} className="link">
 					InSight Mission
 				</Link>
 			</Col>
