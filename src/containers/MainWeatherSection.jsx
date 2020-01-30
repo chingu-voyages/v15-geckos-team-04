b89e0f22bff8596c64c2e7d3adb50ecd6bf97c50
user <ovg43@yandex.ru>
2020-01-30 17:23:18 +0300

    add Recharts to read.me

--- src/containers/MainWeatherSection.jsx
+++ src/containers/MainWeatherSection.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import defaultData from "../data/defaultData";
-import CurrentWeather from './CurrentWeather'
+import CurrentWeather from "./CurrentWeather";
 import SevenDaysForecast from "./SevenDaysForecast";
-import LoadingSpinner from "../components/loadingSpinner"
+import LoadingSpinner from "../components/loadingSpinner";
 
-const {sol_keys} = defaultData;
-const defData = Object.values(defaultData).filter(i=>i.AT);
+const { sol_keys } = defaultData;
+const defData = Object.values(defaultData).filter(i => i.AT);
 
 class MainWeatherSection extends Component {
   constructor(props) {
@@ -15,51 +15,59 @@ class MainWeatherSection extends Component {
       sol: sol_keys,
       isLoaded: false,
       error: null
-    }
-  };
- 
-  
- componentDidMount() {
-   fetch(
-    process.env.REACT_APP_INSIGHT_MARS_WEATHER_API
-)
-  .then(response => response.json())
-  .then(
-    result => {
-      this.setState({
-        isLoaded: true,
-        apiData: Object.values(result).filter(i=>i.AT),
-        apiSol: result.sol_keys
-      });
-      console.log(this.state);
-    },
-    error => {
-      this.setState({
-        isLoaded: true,
-        error
-      });
-    }
-  );}
+    };
+  }
+
+  componentDidMount() {
+    fetch(process.env.REACT_APP_INSIGHT_MARS_WEATHER_API)
+      .then(response => response.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            apiData: Object.values(result).filter(i => i.AT),
+            apiSol: result.sol_keys
+          });
+          console.log(this.state);
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
 
   render() {
-      const {defData, sol, error,isLoaded, apiData, apiSol} = this.state;
+    const { defData, sol, error, isLoaded, apiData, apiSol } = this.state;
 
-      if (error) {
-        return (
-        <div> Error: {error.message}
-          <CurrentWeather data={defData[defData.length - 1]} sol = {sol[sol.length - 1]}/>
-          <SevenDaysForecast data = {defData} sol = {sol} />
-          </div>)
-      } else if (!isLoaded) {
-        return <LoadingSpinner />;
-      } else {
-        
-      return ( 
+    if (error) {
+      return (
         <div>
-          <CurrentWeather data={apiData[apiData.length - 1]} sol = {apiSol[apiSol.length - 1]}/>
-          <SevenDaysForecast data = {apiData} sol = {apiSol} />
+          {" "}
+          Error: {error.message}
+          <CurrentWeather
+            data={defData[defData.length - 1]}
+            sol={sol[sol.length - 1]}
+          />
+          <SevenDaysForecast data={defData} sol={sol} />
         </div>
       );
-  }}}
+    } else if (!isLoaded) {
+      return <LoadingSpinner />;
+    } else {
+      return (
+        <div>
+          <CurrentWeather
+            data={apiData[apiData.length - 1]}
+            sol={apiSol[apiSol.length - 1]}
+          />
+          <SevenDaysForecast data={apiData} sol={apiSol} />
+        </div>
+      );
+    }
+  }
+}
 
-  export default MainWeatherSection;
+export default MainWeatherSection;
