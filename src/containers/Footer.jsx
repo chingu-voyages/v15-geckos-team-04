a84798d0e682d8de1c665de79287d7b423e48658
Aloys Berger <aloysberger@gmail.com>
2020-01-30 20:51:58 +0100

    Set component heights

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer className="app-footer container-fluid text-center">
+        <footer id = 'footer' className="app-footer container-fluid text-center">
         <span> Coded by Geckos Team-04 | 2020 | Chingu Voyage 15</span>
       </footer>
     );
