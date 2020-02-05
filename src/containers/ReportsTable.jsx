1bb6377b0f72ca440e5a60be8e4fd2c07ed72d32
Ana Segarra <anasegarraber@gmail.com>
2020-02-05 17:47:02 +0100

    Fix bug that changed data order

--- src/containers/ReportsTable.jsx
+++ src/containers/ReportsTable.jsx
@@ -4,11 +4,11 @@ import Row from '../components/reportsRow';
 import Button from '../components/scaleButton'
 
 const ReportsTable = (props)=>{
-    const formatedTime = props.data.reverse().map(item => {
+    const formatedTime = [...props.data].reverse().map(item => {
         return props.getDate(item.First_UTC)        
     });
-    const sol = props.sol.reverse();
-    const tableRows = props.data.reverse().map((item, index)=>{
+    const sol = [...props.sol].reverse();
+    const tableRows = [...props.data].reverse().map((item, index)=>{
        return <Row key={sol[index]} time={formatedTime[index]} sol={sol[index]} maxTemp={Math.floor(item.AT.mx)} avTemp={Math.floor(item.AT.av)} minTemp={Math.floor(item.AT.mn)} maxPre={Math.floor(item.PRE.mx)} avPre={Math.floor(item.PRE.av)} minPre={Math.floor(item.PRE.mn)}/>
     });
     
@@ -18,7 +18,7 @@ const ReportsTable = (props)=>{
                 <thead>
                         <tr>
                             <th colSpan="2">Time</th>
-                            <th colSpan="3">Temperature <Button onClick={props.onClick} scale={props.scale}/></th>
+                            <th colSpan="3">Temperature <Button variant="outline-dark" onClick={props.onClick} scale={props.scale}/></th>
                             <th colSpan="3">Pressure (Pa)</th>
                         </tr>
                 </thead>
